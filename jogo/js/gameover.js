038d677a1a1337f7fd1c611f2d0e0b1a5b135f15
GabrielGaudi <gabriel.rosalvo@etec.sp.gov.br>
2024-11-11 21:54:05 -0300

    Que a assombração do javascript seja expurgada da minha vida

--- jogo/js/gameover.js
+++ jogo/js/gameover.js
@@ -1,16 +1,22 @@
-let score = 0;
-
-
+let GameScore = score
 function selectTile() {
     if (gameOver) {return;}
-
+    
     if(this == BabyLeviatanTile){
         if(score == 90){
             currentDifficulty();
         }
         score += 10;
-        document.getElementById("PontoFim").innerText = score.toString();
+        document.getElementById("score").innerText = "Pontuação: " + score.toString();
         BabyLeviatanTile = 
         BabyLeviatanTile.innerHTML = "";
+        GameScore = score
+        console.log(GameScore)
+        }
+    if(this == LeviatanTile) {
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString(); 
+        gameOver = true;
+        window.location.href = "derrota.html"
     }
 }
+
--- jogo/js/jogosonic.js
+++ jogo/js/jogosonic.js
@@ -4,16 +4,21 @@ let score = 0;
 let gameOver = false;
 let gameTiles = 9;
 let time = 30;
+let id = "gameOverScreen";
 
 window.onload = function() {
+    if(document.getElementsByClassName("h2").innerText == "Pontuação: 0"){
+        console.log("Eu criei um monstro")
+    }
     setGame();
     document.querySelectorAll('.square').forEach(item => {
         item.addEventListener('click', selectTile)
       })
-
+      console.log("é sim")
 }
 
 function setGame(){
+    
     setInterval(timer, 1000);
     setInterval(setBaby, 1000);
     setInterval(setLeviatan, 2000);
@@ -26,17 +31,11 @@ function timer(){
     if(time < 0){
         document.getElementById("score").innerText = "GAME OVER: " + score.toString(); 
         gameOver = true;
-        window.location.href = "derrota.html"
+        createGameOver();
     }
 }
 
 function currentDifficulty(){
-
-    difficulty = 2;
-    console.log("primeiro");
-
-if(difficulty == 2){
-    console.log("segundo");
     for(let i = 9; i < 12; i++){
         let square = document.createElement("div");
         square.id = i.toString();
@@ -44,15 +43,15 @@ if(difficulty == 2){
         square.addEventListener("click", selectTile);
 
         document.getElementById("tela").appendChild(square);
+        console.log(square)
 
     }
     gameTiles = 12;
     document.getElementById("tela").style.width = "840px";
-    document.body.style.backgroundImage = 'url(../img/fundo1.jpeg)'
+    document.body.style.backgroundImage = "url(../jogo/img/fundo1.jpeg)"
     document.body.style.color = 'white'
-    console.log("eba")
 }
-}
+
 
 
 function getRandomTile() {
@@ -93,25 +92,56 @@ function setLeviatan() {
     }
     LeviatanTile = document.getElementById(num);
     LeviatanTile.appendChild(leviatan);
+    console.log(score)
 }
 
 function selectTile() {
     if (gameOver) {return;}
-
+    
     if(this == BabyLeviatanTile){
         if(score == 90){
             currentDifficulty();
         }
         score += 10;
-        document.getElementById("score").innerText = score.toString();
+        document.getElementById("score").innerText = "Pontuação: " + score.toString();
         BabyLeviatanTile = 
         BabyLeviatanTile.innerHTML = "";
-        
-
-    }
-    else if (this == LeviatanTile) {
+        GameScore = score
+        console.log(GameScore)
+        }
+    if(this == LeviatanTile) {
         document.getElementById("score").innerText = "GAME OVER: " + score.toString(); 
         gameOver = true;
-        window.location.href = "derrota.html"
+        createGameOver();
     }
 }
+
+function createGameOver(){
+    let screen = document.createElement("div");
+    screen.id = "gameOverScreen";
+    screen.className = "gameOverClass";
+    screen.style.backgroundImage = "url(../jogo/img/fundo3.jpeg)"
+    screen.style.margin = "0"
+    screen.style.width = "400px"
+    screen.style.height = "300px"
+    screen.style.position = "absolute"
+    screen.style.left = "0"
+    screen.style.right = "0"
+    screen.style.marginInline = "auto"
+    screen.style.top = "180px"
+    screen.textContent = "Game Over!" + score;
+    screen.style.fontSize = "50px"
+    document.getElementById("space").appendChild(screen);
+}
+
+/*let square = document.createElement("div");
+square.id = i.toString();
+square.className = "square";
+
+document.getElementById("tela").appendChild(square);
+
+position: absolute; 
+  left: 0; 
+  right: 0; 
+  margin-inline: auto; 
+  width: fit-content;*/
